6d56b1ada94565464d7df810c2ee18432da97899
Antoine Megarbane <am692@st-andrews.ac.uk>
2024-02-28 14:46:01 +0000

    New attempt to fix getReturnAddress

--- src/StackFrame.c
+++ src/StackFrame.c
@@ -28,8 +28,7 @@ unsigned long getBasePointer() {
 
 unsigned long getReturnAddress() {
     unsigned long address;
-    asm volatile("pop %%rsp;"
-        "movq %%rsp, %0;" : "=r"(address));
+    asm volatile("popq %%rsp, %0;" : "=r"(address));
     return address;
 }
 
