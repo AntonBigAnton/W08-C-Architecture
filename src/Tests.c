3a2deb2e91c606138d06cb2ccd51bd9cb38be156
Antoine Megarbane <am692@st-andrews.ac.uk>
2024-03-08 14:23:01 +0000

    Added a Tests.c file for testing

---
+++ src/Tests.c
@@ -0,0 +1,21 @@
+/*
+ * Tests.c
+ *
+ * Test file that calls the executeFactorial function multiple times with different DEFAULT_VALUE_OF_N.
+ *
+ */
+
+#include <stdio.h>
+#include "StackFrame.h"
+#include "Factorial.h"
+
+/*
+ * Only testing method, that contains all tests.
+ *
+ * @return 0
+ */
+int main() {
+
+    executeFactorial();
+    return 0;
+}
