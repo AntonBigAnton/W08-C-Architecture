93f2d623a993669e0154c4766d1c4edd6a9ea191
Antoine Megarbane <am692@st-andrews.ac.uk>
2024-03-10 13:18:22 +0000

    Deleted TestFunctions.c and Tests.h, and modified Test.c and Makefile

--- src/Tests.c
+++ src/Tests.c
@@ -7,7 +7,45 @@
 
 #include <stdio.h>
 #include "StackFrame.h"
-#include "TestFunctions.h"
+
+/*
+ * Static (akin to "private") function to this module.
+ * This function is only ever called by executeFactorialTest below.
+ * When it has reached the final recursive call, it
+ * calls printStackFrames() to print out stack frame data.
+ *
+ */
+
+static unsigned long factorialTest(unsigned long default, unsigned long n, unsigned long accumulator) {
+
+    if (n <= 1) {
+        printStackFrames(default + 1);
+        return accumulator;
+    }
+    return factorialTest(default, n - 1, n * accumulator);
+}
+
+/*
+ * Static (akin to "private") function to call factorialTest with set parameter value.
+ * and print out the result of calling getBasePointer and getReturnAddress.
+ *
+ */
+
+static void executeFactorialTest(unsigned long default) {
+    unsigned long basePointer = getBasePointer();
+    printf("executeFactorial: basePointer = %lx\n", basePointer);
+
+    unsigned long returnAddress = getReturnAddress();
+    printf("executeFactorial: returnAddress = %lx\n", returnAddress);
+
+    printf("executeFactorial: about to call factorial which should print the stack\n\n");
+
+    unsigned long result = 0l;
+    unsigned long number = default;
+    unsigned long accumulator = 1l;
+    result = factorialTest(default, number, accumulator);
+    printf("executeFactorial: factorial(%lu) = %lu\n", number, result);
+}
 
 /*
  * Only testing method, that contains all tests.
